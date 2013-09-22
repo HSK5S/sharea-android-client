d140e06793543ba1d827a6d6fdee0ff95cc171b4
haripo <ehutoad@gmail.com>
2013-09-22 21:34:57 +0900

    Initial commit

---
+++ src/com/example/sharea_android_client/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sharea_android_client;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public FragmentManager getSupportFragmentManager() {
+        return null;
+    }
+}
